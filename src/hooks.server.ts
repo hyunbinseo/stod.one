c92b9bfe768a9feff088e757dd622c3fda9335da
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-12-17 01:35:56 +0900

    chore: format codebase with prettier

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (({ event, resolve }) => {
-  if (event.url.pathname === '/')
-    return Response.redirect('https://github.com/hyunbinseo/stod.one#readme', 302)
+	if (event.url.pathname === '/')
+		return Response.redirect('https://github.com/hyunbinseo/stod.one#readme', 302);
 
-  return resolve(event);
+	return resolve(event);
 }) satisfies Handle;
--- src/routes/[id=mastodon]/+page.ts
+++ src/routes/[id=mastodon]/+page.ts
@@ -2,6 +2,6 @@ import { mastodonId } from '$lib/regex';
 import type { PageLoad } from './$types';
 
 export const load = (({ params: { id } }) => {
-  const [, username, domain] = id.match(mastodonId) || [];
-  return { username, domain, baseUrl: `https://${domain}` }
+	const [, username, domain] = id.match(mastodonId) || [];
+	return { username, domain, baseUrl: `https://${domain}` };
 }) satisfies PageLoad;
