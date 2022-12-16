c8dfe7741c4968d0702d9acbe38ceca61a67aacf
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-12-16 17:46:29 +0900

    feat: redirect text and countdown

--- src/routes/[id=mastodon]/+page.svelte
+++ src/routes/[id=mastodon]/+page.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
-	import { mastodonId } from '$lib/regex';
+	import { onDestroy } from 'svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let intervalId: number;
+	let count = 3;
 
 	if (browser) {
-		const [, username, domain] = $page.url.pathname.substring(1).match(mastodonId) || [];
-		goto(`https://${domain}/@${username}`);
+		intervalId = window.setInterval(() => {
+			if (count) count -= 1;
+			if (!count) goto(data.baseUrl, { replaceState: true });
+		}, 1000);
 	}
+
+	onDestroy(() => {
+		clearTimeout(intervalId);
+	});
 </script>
+
+<span>
+	Redirecting to <a href="{data.baseUrl}/@{data.username}">@{data.username}</a> at
+	<a href={data.baseUrl}>{data.domain}</a> in {count}
+</span>
---
+++ src/routes/[id=mastodon]/+page.ts
@@ -0,0 +1,7 @@
+import { mastodonId } from '$lib/regex';
+import type { PageLoad } from './$types';
+
+export const load = (({ params: { id } }) => {
+  const [, username, domain] = id.match(mastodonId) || [];
+  return { username, domain, baseUrl: `https://${domain}` }
+}) satisfies PageLoad;
