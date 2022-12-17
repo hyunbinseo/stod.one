7df353c313d2db74706641fd58d7fe8d9f14fe10
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2022-12-17 11:07:23 +0900

    feat: create footer with github links

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
+import { gitHubRepoUrl } from '$lib/miscellaneous';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (({ event, resolve }) => {
-	if (event.url.pathname === '/')
-		return Response.redirect('https://github.com/hyunbinseo/stod.one#readme', 302);
+	if (event.url.pathname === '/') return Response.redirect(`${gitHubRepoUrl}#readme`, 302);
 
 	return resolve(event);
 }) satisfies Handle;
---
+++ src/lib/miscellaneous.ts
@@ -0,0 +1,2 @@
+export const gitHubRepoUrl = 'https://github.com/hyunbinseo/stod.one';
+export const gitHubUserUrl = 'https://github.com/hyunbinseo';
